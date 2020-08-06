fbdc887767032acfc68175d8750a378a9651fda0
Jefferson Frade <jefferrson.frade@gmail.com>
2020-08-06 18:18:56 -0300

    Exercício Conversor de Temperatura

---
+++ ex-02.php
@@ -0,0 +1,13 @@
+<?php
+
+// Conversor de Temperatura
+
+$celsius = 100;
+$kelvin = $celsius + 273;
+$fahrenheit = ($celsius * 9 / 5) + 32;
+
+echo 'Temperatura em Celsius: ' . $celsius . 'ºC';
+echo '<hr/>';
+echo 'Temperatura em Kelvin: ' . $kelvin . ' K';
+echo '<hr/>';
+echo 'Temperatura em Fahrenheit: ' . $fahrenheit . 'ºF';
